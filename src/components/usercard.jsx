0cdf9f99e90560257ae12c05b1676082caa8d511
varshapanda <varsha.vijayashwini@kalvium.community>
2025-03-23 10:11:11 +0530

    completed

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,21 +1,26 @@
-// write the component code here
-import React from 'react';
+// import React from 'react';
+import './usercard.css';
 
-const Usercard = ()=>{
+const Usercard = () => {
     const profilePhoto = "https://i.pinimg.com/736x/6f/cf/80/6fcf8083163eb167660a4a1dc182fa30.jpg";
     const name = "Namra";
     const email = "namra@allofusaredead.com";
     const phoneNumber = "1276-543-210";
     const address = "Zombie High School, Hyosan, South Korea";
 
-    return(
+    return (
         <div className="user-card">
-            <img src = {profilePhoto} alt="Profile" style={{ width: "300px", height: "300px" }}/>
-                <h2 >{name}</h2>
-                <p>{email}</p>
-                <p >{phoneNumber}</p>
-                <p>{address}</p>
+            <div className="image-container">
+                <img src={profilePhoto} alt="Profile" className="profile-image" />
+            </div>
+            <h2 className="user-name">{name}</h2>
+            <div className="user-details">
+                <p className="user-info email">{email}</p>
+                <p className="user-info phone">{phoneNumber}</p>
+                <p className="user-info address">{address}</p>
+            </div>
         </div>
-    )
-}
+    );
+};
+
 export default Usercard;
